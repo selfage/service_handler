10c5f11b358af2b92352e7a23970efa588a1f4bf
Yuxuan Xie <teststaybaka@gmail.com>
2021-09-26 04:49:16 +0000

    Fix wildcard for OPTIONS request.

--- preflight_handler.ts
+++ preflight_handler.ts
@@ -1,7 +1,7 @@
 import express = require("express");
 
 export function registerCorsAllowedPreflightHandler(app: express.Express) {
-  app.options("/.*", (req, res) => {
+  app.options("/*", (req, res) => {
     // Allow all.
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "*");
